e2b412720c383a900e7e979673bfe15288662512
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-06 19:48:23 +0200

    feat: close menu when click out of it

--- script/script.js
+++ script/script.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const mainMenu = document.getElementById('main-menu');
     const siteTitle = document.querySelector('.site-title');
 
-    menuToggle.addEventListener('click', function () {
+    menuToggle.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent the click event from propagating to document
         mainMenu.classList.toggle('active');
     });
 
@@ -28,5 +29,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 img.style.transform = 'scale(1)';
             }
         });
+    });
+
+    // Add a click event listener to the document to close the menu when clicking outside
+    document.addEventListener('click', function (event) {
+        if (!event.target.closest('#main-menu')) {
+            // Check if the clicked element or any of its ancestors is not part of the menu
+            mainMenu.classList.remove('active'); // Close the menu
+        }
     });
 });
