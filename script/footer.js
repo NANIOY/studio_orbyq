9e8ab5a69949ce5bb52f5e186de71c4cf9e1301d
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-15 14:36:52 +0200

    feat: recreate footer in javascript

---
+++ script/footer.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function createFooter() {
+        const footer = document.createElement('footer');
+        footer.classList.add('footer');
+
+        const logoLink = document.createElement('a');
+        logoLink.href = 'https://studio-orbyq.com/contact';
+        logoLink.classList.add('footer-logo-link');
+
+        const logoImage = document.createElement('img');
+        logoImage.src = '../../images/logo/logomark.svg';
+        logoImage.alt = 'logomark-white';
+        logoImage.classList.add('footer-logo-image');
+        logoImage.setAttribute('loading', 'lazy');
+
+        logoLink.appendChild(logoImage);
+
+        const contactInfo = document.createElement('div');
+        contactInfo.classList.add('contact-info');
+
+        const contactTitle = document.createElement('h3');
+
+        const contactLink = document.createElement('a');
+        contactLink.href = 'https://studio-orbyq.com/contact';
+        contactLink.classList.add('contact-link');
+        contactLink.textContent = "Let's Talk";
+
+        contactTitle.appendChild(contactLink);
+        contactInfo.appendChild(contactTitle);
+
+        const socialMedia = document.createElement('div');
+        socialMedia.classList.add('social-media');
+
+        const socialMediaList = document.createElement('ul');
+        socialMediaList.classList.add('social-media-list');
+
+        const socialMediaItems = [
+            { href: 'https://www.behance.net/studio-orbyq', alt: 'Behance' },
+            { href: 'https://dribbble.com/Studio-Orbyq', alt: 'Dribbble' },
+            { href: 'https://letterboxd.com/NANIO/', alt: 'Letterboxd' },
+        ];
+
+        socialMediaItems.forEach(item => {
+            const socialMediaItem = document.createElement('li');
+            socialMediaItem.classList.add('social-media-item');
+
+            const socialMediaLink = document.createElement('a');
+            socialMediaLink.href = item.href;
+            socialMediaLink.classList.add('social-media-link');
+            socialMediaLink.target = '_blank';
+
+            const socialMediaIcon = document.createElement('img');
+            socialMediaIcon.src = `../../images/footer/${item.alt}.svg`;
+            socialMediaIcon.alt = item.alt;
+            socialMediaIcon.classList.add('social-media-icon');
+
+            socialMediaLink.appendChild(socialMediaIcon);
+            socialMediaItem.appendChild(socialMediaLink);
+            socialMediaList.appendChild(socialMediaItem);
+        });
+
+        socialMedia.appendChild(socialMediaList);
+
+        footer.appendChild(logoLink);
+        footer.appendChild(contactInfo);
+        footer.appendChild(socialMedia);
+
+        document.body.appendChild(footer);
+    }
+
+    createFooter();
+});
