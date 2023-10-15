a404c1b17e29db5bdf69595dbabed33f5aa779e0
Yannick Van den Brande <yavadebr@gmail.com>
2023-10-15 20:19:12 +0200

    feat: apply loading to back as well

--- script/loading.js
+++ script/loading.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Create the loading screen container
-    const loadingScreen = document.createElement('div');
-    loadingScreen.classList.add('loading-screen');
+    const loadingScreen = createLoadingScreen();
     document.body.appendChild(loadingScreen);
 
-    // Simulate a loading period and hide the loading screen
+    window.addEventListener('popstate', function (event) {
+        if (event.state && event.state.pageName === 'loaded') {
+            // Simulate a loading period and hide the loading screen
+            loadingScreen.style.display = 'block';
+            setTimeout(function () {
+                loadingScreen.style.opacity = 0;
+                loadingScreen.addEventListener('transitionend', function () {
+                    loadingScreen.style.display = 'none';
+                }, { once: true });
+            }, 0);
+        }
+    });
+
+    // Simulate the initial loading
+    loadingScreen.style.display = 'block';
     setTimeout(function () {
-        loadingScreen.style.opacity = 0; // Start the fade-out effect
+        loadingScreen.style.opacity = 0;
         loadingScreen.addEventListener('transitionend', function () {
-            loadingScreen.style.display = 'none'; // Hide the loading screen after the transition
+            loadingScreen.style.display = 'none';
+            // After the initial loading, set the page name for popstate
+            window.history.replaceState({ pageName: 'loaded' }, '');
         }, { once: true });
     }, 0);
 });
+
+function createLoadingScreen() {
+    const loadingScreen = document.createElement('div');
+    loadingScreen.classList.add('loading-screen');
+    return loadingScreen;
+}
